bb22a3a77a27c53235fcb30d1dcca0e73276eb23
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-09 15:44:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,17 +12,9 @@ const START = document.getElementById('start')
 
 var gameInterval = null
 
-/**
- * Be aware of what's above this line,
- * but all of your work should happen below.
- */
-
 function checkCollision(rock) {
   const top = positionToInteger(rock.style.top);
 
-  // rocks are 20px high
-  // DODGER is 20px high
-  // GAME_HEIGHT - 20 - 20 = 360px;
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left);
     const dodgerRightEdge = dodgerLeftEdge + 40;
@@ -44,22 +36,14 @@ function createRock(x) {
   rock.className = 'rock'
   rock.style.left = `${x}px`
 
-  // Hmmm, why would we have used `var` here?
   var top = 0
 
   rock.style.top = `${top}px`
 
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
   GAME.appendChild(rock);
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
+
   function moveRock() {
-    // implement me!
+
      top += 2;
      rock.style.top = `${top}px`
      
@@ -72,38 +56,16 @@ function createRock(x) {
      if (top >= GAME_HEIGHT) {
        GAME.removeChild(rock);
      }
-    
-     
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
 
   }
 
-
-  // We should kick of the animation of the rock around here
   window.requestAnimationFrame(moveRock);
 
-  // Add the rock to ROCKS so that we can remove all rocks
-  // when there's a collision
   ROCKS.push(rock)
 
-  // Finally, return the rock element you've created
   return rock
 }
 
-/**
- * End the game by clearing `gameInterval`,
- * removing all ROCKS from the DOM,
- * and removing the `moveDodger` event listener.
- * Finally, alert "YOU LOSE!" to the player.
- */
 function endGame() {
   alert("YOU LOSE!");
   clearInterval(gameInterval);
@@ -115,14 +77,7 @@ function endGame() {
 }
 
 function moveDodger(e) {
-  // implement me!
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
+
      if(e.which == LEFT_ARROW){
        e.preventDefault();
        e.stopPropagation();
